7ac1e8e94c7f8edacc1bf9498145aabb968e0634
RokoLolic <73552688+RokoLolic@users.noreply.github.com>
2020-11-03 21:45:07 +0100

    Update Zadatak3.c
    Ne mogu napravit upisivanje. zao mi je.

--- Zadatak3.c
+++ Zadatak3.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #pragma warning(disable:4996)
 typedef struct osoba oso;
 typedef struct osoba * poz;
@@ -19,11 +16,13 @@ int ispis(poz);
 int meni(poz);
 int trazi(poz);
 int brisi(poz);
-poz trazipret(poz, char []);
-poz trazinakon(poz, char []);
+poz trazipret(poz, char[]);
+poz trazinakon(poz, char[]);
 int upisprije(poz);
 int upisnakon(poz);
 int sort(poz);
+int datci(poz);
+int datup(poz);
 int main() {
 	struct osoba head;
 	head.next = NULL;
@@ -32,39 +31,48 @@ int main() {
 }
 int meni(poz p) {
 	puts("Sto zelite uciniti");
-	puts("P za upis na prvo mijesto, K za upis na kraj, I za ispis, Z za ugasiti program, B za brisati, N za unos nakon elementa, S za unos ispred nekog elementa");
+	puts("P za upis na prvo mijesto, K za upis na kraj, I za ispis, Z za ugasiti program, B za brisati, N za unos nakon elementa, S za unos ispred nekog elementa, O za sortirati, C za citati iz datoteke, U za upisati u datoteku");
 	int Z = 0;
 	char a[1];
-	while (Z==0) 
+	while (Z == 0)
 	{
 		scanf("%s", a);
-		switch(*a) {
-			case 'P':
-				upisp(p);
-				break;
-			case 'K':
-				upisk(p);
-				break;
-			case 'I':
-				ispis(p);
-				break;
-			case 'Z':
-				Z = 1;
-				break;
-			case 'T':
-				trazi(p);
-				break;
-			case 'B':
-				brisi(p);
-				break;
-			case 'N':
-				upisnakon(p);
-				break;
-			case 'S':
-				upisprije(p);
-				break;
-			default:
-				puts("P za upis na prvo mijesto, K za upis na kraj, I za ispis, Z za ugasiti program, B za brisati, N za unos nakon elementa, S za unos ispred nekog elementa");
+		switch (*a) {
+		case 'P':
+			upisp(p);
+			break;
+		case 'K':
+			upisk(p);
+			break;
+		case 'I':
+			ispis(p);
+			break;
+		case 'Z':
+			Z = 1;
+			break;
+		case 'T':
+			trazi(p);
+			break;
+		case 'B':
+			brisi(p);
+			break;
+		case 'N':
+			upisnakon(p);
+			break;
+		case 'S':
+			upisprije(p);
+			break;
+		case 'O':
+			sort(p);
+			break;
+		case 'C':
+			datci(p);
+			break;
+		case 'U':
+			datup(p);
+			break;
+		default:
+			puts("P za upis na prvo mijesto, K za upis na kraj, I za ispis, Z za ugasiti program, B za brisati, N za unos nakon elementa, S za unos ispred nekog elementa, O za sortirati, C za citati iz datoteke, U za upisati u datoteku");
 		}
 	}
 	return 0;
@@ -109,7 +117,7 @@ int ispis(poz p) {
 }
 int trazi(poz p) {
 	char b[10];
-	int i=0;
+	int i = 0;
 	puts("Upisi prezime koje trazis");
 	scanf("%s", b);
 	while ((p->next != NULL) && (p->prezime != b)) {
@@ -124,37 +132,37 @@ int brisi(poz p) {
 	poz temp, prev;
 	puts("Upisi prezime clana koje brises");
 	scanf("%s", b);
-	prev=trazipret(p, b);
+	prev = trazipret(p, b);
 	temp = prev->next;
 	prev->next = temp->next;
 	free(temp);
 	return 1;
 }
-poz trazinakon(poz q, char b[]){
-	while ((q->next != NULL) && (strcmp(q->prezime, b)!=0)) {
+poz trazinakon(poz q, char b[]) {
+	while ((q->next != NULL) && (strcmp(q->prezime, b) != 0)) {
 		q = q->next;
 	}
-	if(q->next!=NULL)
-		q=q->next;
+	if (q->next != NULL)
+		q = q->next;
 	printf("sljedeci je %s\n", q->prezime);
 	return q;
 }
 poz trazipret(poz q, char b[]) {
-	while ((q->next != NULL) && (strcmp(q->next->prezime, b)!=0)) {
+	while ((q->next != NULL) && (strcmp(q->next->prezime, b) != 0)) {
 		q = q->next;
 	}
 	printf("prethodni je %s\n", q->prezime);
 	return q;
 }
-int upisprije(poz p){
+int upisprije(poz p) {
 	poz q, prev;
 	char b[10];
 	q = (poz)malloc(sizeof(oso));
 	puts("Upisi prije kojeg prezimena upisujes");
 	scanf("%s", b);
-	prev=trazipret(p, b);
-	q->next=prev->next;
-	prev->next=q;
+	prev = trazipret(p, b);
+	q->next = prev->next;
+	prev->next = q;
 	puts("Prvo upiši ime, prezime i na kraju godinu, sve u svojem retku");
 	scanf("%s", b);
 	strcpy(q->ime, b);
@@ -163,15 +171,17 @@ int upisprije(poz p){
 	scanf("%d", &(q->god));
 	return 1;
 }
-int upisnakon(poz p){
-	poz q, nak;
+int upisnakon(poz p) {
+	poz q;
 	char b[10];
 	q = (poz)malloc(sizeof(oso));
+	puts("Nakon kojeg upisujes?");
+	scanf("%s", b);
 	while ((p->next != NULL) && (p->prezime != b)) {
 		p = p->next;
 	}
-	q->next=p->next;
-	p->next=q;
+	q->next = p->next;
+	p->next = q;
 	puts("Prvo upiši ime, prezime i na kraju godinu, sve u svojem retku");
 	scanf("%s", b);
 	strcpy(q->ime, b);
@@ -180,8 +190,57 @@ int upisnakon(poz p){
 	scanf("%d", &(q->god));
 	return 1;
 }
-int swap(poz q, poz p){
-    
+int datci(poz p){
+	FILE *fp;
+	fp = fopen("student.txt", "r");
+	if (fp == NULL) {
+		puts("Datoteka nije otvorena");
+	}
+	while (feof(fp) == 0) {
+		poz q;
+		q = (poz)malloc(sizeof(oso));
+		fscanf(fp, "%s", q->ime);
+		fscanf(fp, "%s", q->prezime);
+		fscanf(fp, "%d", &q->god);
+		q->next = p->next;
+		p->next = q;
+	}
+	return 1;
+}
+int datup(poz p) {
+	FILE *fp;
+	fp = fopen("meta.txt", "w");
+	if (fp == NULL) {
+		puts("Datoteka nije otvorena");
+	}
+	while (p->next!=NULL){
+		p = p->next;
+		fprintf(fp, "%s", p->ime);
+		fprintf(fp, "%s", p->prezime);
+		fprintf(fp, "%d", p->god);
+	}
+	return 1;
+}
+int sort(poz p) {
+	poz prev, i, t, kr;
+	kr = NULL;
+	while (p->next != kr) {
+		prev = p;
+		i = p->next;
+		while (i->next != kr) {
+			if (strcmp(i->prezime, i->next->prezime)>0) {
+				t = i->next;
+				prev->next = t;
+				i->next = t->next;
+				t->next = i;
+
+				i = t;
+			}
+			prev = i;
+			i = i->next;
+		}
+		kr = i;
+	}
+	return 0;
 }
 
-}
