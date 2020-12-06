92b178a0be71b24547e0ca003a6f13fe3065eb52
RokoLolic <73552688+RokoLolic@users.noreply.github.com>
2020-12-06 19:09:56 +0100

    Update zadatak6.c

--- zadatak6.c
+++ zadatak6.c
@@ -6,53 +6,66 @@
 #define b 100
 
 struct cvor;
-typedef struct cvor CVOR; 
+typedef struct cvor CVOR;
 typedef struct cvor * poz;
-struct cvor{
+struct cvor {
 	int br;
 	poz next;
 };
-int push(poz);
+int pushr(poz*, poz);
+int pushs(poz);
 int pop(poz);
 
-int main(){
+int main() {
 	CVOR red;
 	CVOR stog;
 	poz last;
-	last=&red;
-	red.next=NULL;
-	stog.next=NULL;
+	last = &red;
+	red.next = NULL;
+	stog.next = NULL;
 	srand((unsigned)(time(NULL)));
 	puts("Red");
-	push(last);
-	push(last);
-	push(last);
-	push(last);
+	pushr(&last, &red);
+	pushr(&last, &red);
+	pushr(&last, &red);
+	pushr(&last, &red);
 	pop(&red);
 	pop(&red);
 	pop(&red);
+	pop(&red);
+	pushr(&last, &red);
 	puts("Stog");
-	push(&stog);
-	push(&stog);
-	push(&stog);
+	pushs(&stog);
+	pushs(&stog);
+	pushs(&stog);
 	pop(&stog);
 	pop(&stog);
 	return 0;
 }
-int push(poz l){
+int pushr(poz* l, poz r) {
 	poz q;
-	q=(poz)malloc(sizeof(CVOR));
-	q->br=rand()%(b-a+1)+a;
-	q->next=l->next;
-	l->next=q;
-	l=q;
+	q = (poz)malloc(sizeof(CVOR));
+	q->br = rand() % (b - a + 1) + a;
+	q->next = (**l).next;
+	(**l).next = q;
+	*l = q;
+	printf("PUSH %d\n", q->br);
 	return 1;
 }
-int pop(poz p){
+int pushs(poz l) {
+	poz q;
+	q = (poz)malloc(sizeof(CVOR));
+	q->br = rand() % (b - a + 1) + a;
+	q->next = l->next;
+	l->next = q;
+	printf("PUSH %d\n", q->br);
+	return 1;
+}
+int pop(poz p) {
 	poz temp;
-	temp=p->next;
-	printf("%d", temp->br);
-	p->next=temp->next;
+	temp = p->next;
+	printf("POP %d\n", temp->br);
+	p->next = temp->next;
 	free(temp);
 	return 1;
 }
