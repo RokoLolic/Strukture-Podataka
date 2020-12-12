ac1fb27aa42e723a394e1cc754702e0db0994c65
RokoLolic <73552688+RokoLolic@users.noreply.github.com>
2020-12-12 18:15:11 +0100

    Update zadatak7.c

--- zadatak7.c
+++ zadatak7.c
@@ -1,28 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
-#define m 10
+#include <string.h>
+#include <ctype.h>
+#define m 20
+#pragma warning(disable:4996)
+typedef struct cvor cl;
+typedef struct cvor * poz;
+struct cvor {
+	float br;
+	poz next;
+};
 
-
-typedef 
+float pop(poz);
+int push(poz, float);
 float racun(char *);
-int main(){
-	char s[m];
+int postfix(char **);
+int main() {
+	char *s;
+	s = (char*)malloc(sizeof(cl)*m);
+	float rez;
+	fgets(s, m, stdin);
+	//postfix(&s);
+	rez=racun(s);
+	printf("%f", rez);
+	return 0;
 }
 
-float racun(char* s){
-	int i, n; //i = je li sscanf dobia vrijednost, n=broj bitvoa koji je sscanf procitao
-	float br, a, b;
-	char o; //operacija
-	while(strlen(s)){
-		i = sscanf(s, "%f %n", &br, &n);
-		if(i)
-			push(br);
-		else
-		{
-			a=pop();
-			b=pop();
-			
-		}
+/*int postfix(char **i) {
+	char z[1];
+	int n;
+	char* a, b;
+	cl stog;
+	stog.next = NULL;
+	while (strlen(*i) != 0) {
+		sscanf
 	}
+}*/
+
+float racun(char* s) {
+	int n, i;
+	float a, b, z;
+	char o; //operand
+	cl stog;
+	stog.next = NULL;
+	while (strlen(s) != 0) {
+		i=sscanf(s, "%f %n", &z, &n);
+		if (i) {
+			push(&stog, z);
+		}
+		else {
+			o = s[0];
+			switch (o) {
+			case '+':
+				a = pop(&stog);
+				b = pop(&stog);
+				push(&stog, a + b);
+				break;
+			case '-':
+				a = pop(&stog);
+				b = pop(&stog);
+				push(&stog, b - a);
+				break;
+			case '*':
+				a = pop(&stog);
+				b = pop(&stog);
+				push(&stog, a * b);
+				break;
+			case '/':
+				a = pop(&stog);
+				b = pop(&stog);
+				push(&stog, b / a);
+				break;
+			default:
+				puts("Krivi simbol");
+			}
+		}
+		s += n;
+	}
+	return pop(&stog);
+}
+
+int push(poz p, float x) {
+	poz q;
+	q = (poz)malloc(sizeof(cl));
+	q->next = p->next;
+	p->next = q;
+	q->br = x;
+	return 1;
+}
+
+float pop(poz p) {
+	float x;
+	poz temp;
+	temp = p->next;
+	p->next = temp->next;
+	x = temp->br;
+	free(temp);
+	return x;
+
 }
